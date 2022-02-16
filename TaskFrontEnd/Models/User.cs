bc1bfb01b4045f3d6c84e565c96817ba669bfd68
shivam <shivamnagpal48@gmail.com>
2022-02-16 12:04:10 +0530

    Change Datatype password

--- TaskFrontEnd/Models/User.cs
+++ TaskFrontEnd/Models/User.cs
@@ -15,6 +15,7 @@ namespace TaskFrontEnd.Models
         [Required]
         public string Username { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Token { get; set; }
     }
